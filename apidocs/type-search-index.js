32e78a6715bf67ceafe271cc01b3eb6101c118fb
Rob Spoor <rob@icemanx.nl>
2022-09-03 16:51:29 +0200

    Rebuilt site using Java 8

--- apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.github.robtimus.application.path","l":"ApplicationPath"},{"p":"com.github.robtimus.application.path","l":"UserDataOption"}];updateSearchResults();
