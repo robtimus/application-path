33da3a293ea3c96ffdb33b85fc446d8bfc48469c
Rob Spoor <rob@icemanx.nl>
2022-09-03 16:48:44 +0200

    Initial content

---
+++ apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.github.robtimus.application.path","l":"ApplicationPath"},{"p":"com.github.robtimus.application.path","l":"UserDataOption"}];updateSearchResults();
